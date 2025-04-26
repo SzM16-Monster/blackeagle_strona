55275e0a91c19abf326132d085675d3a82dc8ff5
Feste790 <karolmarciniak790@gmail.com>
2025-04-26 20:36:21 +0200

    Update home.js

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,16 +1,18 @@
+import React from 'react';
+
 function Home({ filteredMovies }) {
     return (
         <div className="movie-grid">
             {filteredMovies.length > 0 ? (
                 filteredMovies.map((movie, i) => (
-                    <div key={movie.id} className="movie-card">                        
-                            <img
-                                src={movie.poster || 'https://via.placeholder.com/200x300'}
-                                alt={movie.title}
-                                className="movie-poster"
-                            />
+                    <div key={movie.movie_id} className="movie-card">
+                        <img
+                            src={movie.poster || 'https://via.placeholder.com/200x300'}
+                            alt={movie.title}
+                            className="movie-poster"
+                        />
                         <h3 className="movieTitle">{movie.title}</h3>
-                        <p>{movie.year}</p>
+                        <p>{movie.prod_year}</p>
                     </div>
                 ))
             ) : (
