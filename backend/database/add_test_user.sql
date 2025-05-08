13a07e9d1334b5f1b92da30f3ef4859f7aa7a418
Feste790 <feste790@gmail.com>
2025-05-08 19:48:39 +0200

    Rejestracja i logowanie

--- backend/database/add_test_user.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO AppUser (appuser_id, first_name, last_name, email, username, password, created_at) VALUES
-(1, 'John', 'Doe', 'john.doe@example.com', 'johndoe', 'hashed_password_123', '2025-04-25'),
-(2, 'Jane', 'Smith', 'jane.smith@example.com', 'janesmith', 'hashed_password_456', '2025-04-25'),
-(3, 'Robert', 'Brown', 'robert.brown@example.com', 'robertbrown', 'hashed_password_789', '2025-04-25');
